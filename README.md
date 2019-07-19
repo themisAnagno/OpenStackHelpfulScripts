aea30689ba141109f03c0d9c32d094fc3fb3539b
themis <thmanagnostopoulos@iit.demokritos.gr>
2019-07-19 17:59:12 +0300

    Openstack image back up script created

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Helpful Scripts
 
 1. Openstack remove security groups
-2. Openstack create flavors
+1. Openstack create flavors
+1. Openstack image back up to local and/or remote server
---
+++ openstack_images_backup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Script to backup images from an Openstack Cloud
+
+image_dir="images_bak"
+mkdir $image_dir && cd $image_dir
+
+openstack image list > os_image_file1.txt
+sleep 2
+num=num=$(wc -l < os_image_file1.txt)
+num=$(( $num-1 ))
+sed -n "4,${num}p" os_image_file1.txt > os_image_file2.txt
+sleep 1
+cut -f 2 -d "|" os_image_file2.txt > os_image_file3.txt
+sleep 1
+cut -f 3 -d "|" os_image_file2.txt > os_image_file4.txt
+
+while read -r f1 && read -r f2 <&3;
+do
+  glance image-download --file $f2 $f1
+  echo "Downloaded $f2"
+  sleep 2
+done < os_image_file3.txt 3<os_image_file4.txt
+
+
+read -p "Transfer images to a remote server? (Warning: This will copy the ssh key to the remote server) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	read -p "Remote server IP: " server_ip
+	read -p "Remote server Username: " server_username
+	read -p "Path to remote server: " server_path
+	echo "Copy ssh key to remote server"
+	ssh-copy-id $server_username@$server_ip || ( ssh-keygen && ssh-copy-id $server_username@$server_ip )
+
+	while IFS= read -r image
+	do
+	  scp $image $server_username@$server_ip:$server_path
+	  sleep 1
+	done < os_image_file4.txt
+
+	read -p "Remove images from local server? " -n 1 -r
+	echo    # (optional) move to a new line
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+	    cd ..
+	    rm -r $image_dir
+	else
+		rm -f os_image_file{1..4}.txt 
+		cd ..
+	fi
+else
+	rm -f os_image_file{1..4}.txt
+	cd ..
+fi
